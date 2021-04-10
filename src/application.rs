f6ba331c1c7523fc16a10ada34538ef5c84948df
Martin Pobaschnig <mpobaschnig@posteo.de>
2021-04-10 11:47:44 +0200

    application: set correct license in about dialog

--- src/application.rs
+++ src/application.rs
@@ -143,7 +143,7 @@ impl VApplication {
         let dialog = gtk::AboutDialogBuilder::new()
             .program_name("Vaults")
             .logo_icon_name(config::APP_ID)
-            .license_type(gtk::License::MitX11)
+            .license_type(gtk::License::Gpl30)
             .website("https://gitlab.gnome.org/mpobaschnig/Vaults/")
             .version(config::VERSION)
             .transient_for(&self.get_main_window())
