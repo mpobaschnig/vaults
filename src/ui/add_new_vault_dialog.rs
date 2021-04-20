73c004af73d1f3ef9ab8a29d1b0973f9d951ffad
Martin Pobaschnig <mpobaschnig@posteo.de>
2021-04-21 01:30:03 +0200

    add/import vault: restructure enable button condition function

--- src/ui/add_new_vault_dialog.rs
+++ src/ui/add_new_vault_dialog.rs
@@ -24,7 +24,7 @@ use gettextrs::gettext;
 use gtk::gio;
 use gtk::{self, prelude::*};
 use gtk::{glib, CompositeTemplate};
-use gtk::{glib::clone, subclass::prelude::*};
+use gtk::{glib::clone, glib::GString, subclass::prelude::*};
 
 use crate::{
     backend::{Backend, AVAILABLE_BACKENDS},
@@ -259,15 +259,22 @@ impl AddNewVaultDialog {
         dialog.show();
     }
 
-    fn check_add_button_enable_conditions(&self) {
+    fn is_valid_vault_name(&self, vault_name: GString) -> bool {
         let self_ = imp::AddNewVaultDialog::from_instance(self);
 
-        let vault_name = self_.vault_name_entry.get_text();
-        let backend = self_.backend_type_combo_box_text.get_active();
-        let password = self_.password_entry.get_text();
-        let confirm_password = self_.password_confirm_entry.get_text();
-        let encrypted_data_directory = self_.encrypted_data_directory_entry.get_text();
-        let mount_directory = self_.mount_directory_entry.get_text();
+        if vault_name.is_empty() {
+            self_
+                .vault_name_action_row
+                .set_subtitle(Some(&gettext("Name is not valid.")));
+            false
+        } else {
+            self_.vault_name_action_row.set_subtitle(Some(""));
+            true
+        }
+    }
+
+    fn is_different_vault_name(&self, vault_name: GString) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
 
         let is_duplicate_name = UserConfig::instance()
             .get_map()
@@ -276,99 +283,164 @@ impl AddNewVaultDialog {
             self_
                 .vault_name_action_row
                 .set_subtitle(Some(&gettext("Name already exists.")));
+            false
         } else {
-            if vault_name.is_empty() {
-                self_
-                    .vault_name_action_row
-                    .set_subtitle(Some(&gettext("Name is not valid.")));
-            } else {
-                self_.vault_name_action_row.set_subtitle(Some(""));
-            }
+            true
         }
+    }
 
-        let mut is_encrypted_data_directory_empty = false;
-        match std::fs::read_dir(encrypted_data_directory.to_string()) {
+    fn is_path_empty(&self, path: &GString) -> Result<bool, std::io::Error> {
+        match std::fs::read_dir(path.to_string()) {
             Ok(dir) => {
                 if dir.count() > 0 {
-                    self_
-                        .encrypted_data_directory_action_row
-                        .set_subtitle(Some(&gettext("Directory is not empty.")));
+                    Ok(false)
                 } else {
-                    is_encrypted_data_directory_empty = true;
-                    self_
-                        .encrypted_data_directory_action_row
-                        .set_subtitle(Some(&gettext("")));
+                    Ok(true)
                 }
             }
             Err(e) => {
-                log::debug!("Could not read encrypted data directory: {}", e);
+                log::debug!("Could not read path {}: {}", path, e);
+                Err(e)
+            }
+        }
+    }
+
+    fn is_encrypted_data_directory_valid(&self, encrypted_data_directory: &GString) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        match self.is_path_empty(encrypted_data_directory) {
+            Ok(is_empty) => {
+                if is_empty {
+                    self_
+                        .encrypted_data_directory_action_row
+                        .set_subtitle(Some(&gettext("")));
+                    true
+                } else {
+                    self_
+                        .encrypted_data_directory_action_row
+                        .set_subtitle(Some(&gettext("Directory is not empty.")));
+                    false
+                }
+            }
+            Err(_) => {
                 self_
                     .encrypted_data_directory_action_row
                     .set_subtitle(Some(&gettext("Directory is not valid.")));
+                false
             }
         }
+    }
 
-        let mut is_mount_directory_empty = false;
-        match std::fs::read_dir(mount_directory.to_string()) {
-            Ok(dir) => {
-                if dir.count() > 0 {
-                    self_
-                        .mount_directory_action_row
-                        .set_subtitle(Some(&gettext("Directory is not empty.")));
-                } else {
-                    is_mount_directory_empty = true;
+    fn is_mount_directory_valid(&self, mount_directory: &GString) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        match self.is_path_empty(mount_directory) {
+            Ok(is_empty) => {
+                if is_empty {
                     self_
                         .mount_directory_action_row
                         .set_subtitle(Some(&gettext("")));
+                    true
+                } else {
+                    self_
+                        .mount_directory_action_row
+                        .set_subtitle(Some(&gettext("Directory is not empty.")));
+                    false
                 }
             }
-            Err(e) => {
-                log::debug!("Could not read mount directory: {}", e);
+            Err(_) => {
                 self_
                     .mount_directory_action_row
                     .set_subtitle(Some(&gettext("Directory is not valid.")));
+                false
             }
         }
+    }
 
-        if is_encrypted_data_directory_empty
-            && is_mount_directory_empty
-            && encrypted_data_directory.eq(&mount_directory)
-        {
+    fn are_directories_different(
+        &self,
+        encrypted_data_directory: &GString,
+        mount_directory: &GString,
+    ) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        if encrypted_data_directory.eq(mount_directory) {
             self_
                 .encrypted_data_directory_action_row
                 .set_subtitle(Some(&gettext("Directories must not be equal.")));
             self_
                 .mount_directory_action_row
                 .set_subtitle(Some(&gettext("Directories must not be equal.")));
-        }
-
-        if !vault_name.is_empty()
-            && !password.is_empty()
-            && !confirm_password.is_empty()
-            && !encrypted_data_directory.is_empty()
-            && !mount_directory.is_empty()
-            && backend.is_some()
-            && !is_duplicate_name
-            && is_encrypted_data_directory_empty
-            && is_mount_directory_empty
-        {
-            if password.eq(&confirm_password) {
-                self_.add_new_vault_button.set_sensitive(true);
-                self_.password_action_row.set_subtitle(Some(""));
-            } else {
-                self_.add_new_vault_button.set_sensitive(false);
-                self_
-                    .password_action_row
-                    .set_subtitle(Some(&gettext("Passwords are not equal!")));
-            }
+            false
         } else {
-            if password.eq(&confirm_password) {
-                self_.password_action_row.set_subtitle(Some(""));
+            true
+        }
+    }
+
+    fn are_passwords_empty(&self, password: &GString, confirm_password: &GString) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        if password.is_empty() && confirm_password.is_empty() {
+            self_
+                .password_action_row
+                .set_subtitle(Some(&gettext("Password is empty")));
+            true
+        } else {
+            self_.password_action_row.set_subtitle(Some(&gettext("")));
+            false
+        }
+    }
+
+    fn are_passwords_equal(&self, password: &GString, confirm_password: &GString) -> bool {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        if password.eq(confirm_password) {
+            self_.password_action_row.set_subtitle(Some(""));
+            true
+        } else {
+            self_
+                .password_action_row
+                .set_subtitle(Some(&gettext("Passwords are not equal!")));
+            false
+        }
+    }
+
+    fn check_add_button_enable_conditions(&self) {
+        let self_ = imp::AddNewVaultDialog::from_instance(self);
+
+        let vault_name = self_.vault_name_entry.get_text();
+        let password = self_.password_entry.get_text();
+        let confirm_password = self_.password_confirm_entry.get_text();
+        let encrypted_data_directory = self_.encrypted_data_directory_entry.get_text();
+        let mount_directory = self_.mount_directory_entry.get_text();
+
+        let is_valid_vault_name = self.is_valid_vault_name(vault_name.clone());
+        let is_different_vault_name = self.is_different_vault_name(vault_name);
+        let is_encrypted_data_directory_valid =
+            self.is_encrypted_data_directory_valid(&encrypted_data_directory);
+        let is_mount_directory_valid = self.is_mount_directory_valid(&mount_directory);
+        let are_directories_different =
+            if is_encrypted_data_directory_valid && is_mount_directory_valid {
+                self.are_directories_different(&encrypted_data_directory, &mount_directory)
             } else {
-                self_
-                    .password_action_row
-                    .set_subtitle(Some(&gettext("Passwords are not equal!")));
-            }
+                false
+            };
+        let are_passwords_empty = self.are_passwords_empty(&password, &confirm_password);
+        let are_passwords_equal = if !are_passwords_empty {
+            self.are_passwords_equal(&password, &confirm_password)
+        } else {
+            false
+        };
+
+        if is_valid_vault_name
+            && is_different_vault_name
+            && is_encrypted_data_directory_valid
+            && is_mount_directory_valid
+            && are_directories_different
+            && are_passwords_equal
+        {
+            self_.add_new_vault_button.set_sensitive(true);
+        } else {
             self_.add_new_vault_button.set_sensitive(false);
         }
     }
