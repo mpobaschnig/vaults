b7bde63f9e39dca8d3f2dc9184177ca5e4fb880a
Martin Pobaschnig <martin.pobaschnig@protonmail.com>
2021-12-25 22:09:22 +0100

    gocryptfs: Use gocryptfs command directly

--- src/backend/gocryptfs.rs
+++ src/backend/gocryptfs.rs
@@ -24,9 +24,7 @@ use std::process::Command;
 use std::{io::Write, process::Stdio};
 
 pub fn is_available() -> Result<bool, BackendError> {
-    let output = Command::new("flatpak-spawn")
-        .arg("--host")
-        .arg("gocryptfs")
+    let output = Command::new("gocryptfs")
         .arg("--version")
         .output()?;
 
@@ -34,11 +32,9 @@ pub fn is_available() -> Result<bool, BackendError> {
 }
 
 pub fn init(vault_config: &VaultConfig, password: String) -> Result<(), BackendError> {
-    let mut child = Command::new("flatpak-spawn")
+    let mut child = Command::new("gocryptfs")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .arg("--host")
-        .arg("gocryptfs")
         .arg("--init")
         .arg("-q")
         .arg("--")
@@ -68,11 +64,9 @@ pub fn init(vault_config: &VaultConfig, password: String) -> Result<(), BackendE
 }
 
 pub fn open(vault_config: &VaultConfig, password: String) -> Result<(), BackendError> {
-    let mut child = Command::new("flatpak-spawn")
+    let mut child = Command::new("gocryptfs")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .arg("--host")
-        .arg("gocryptfs")
         .arg("-q")
         .arg("--")
         .arg(&vault_config.encrypted_data_directory)
@@ -100,10 +94,8 @@ pub fn open(vault_config: &VaultConfig, password: String) -> Result<(), BackendE
 }
 
 pub fn close(vault_config: &VaultConfig) -> Result<(), BackendError> {
-    let child = Command::new("flatpak-spawn")
+    let child = Command::new("fusermount")
         .stdout(Stdio::piped())
-        .arg("--host")
-        .arg("fusermount")
         .arg("-u")
         .arg(&vault_config.mount_directory)
         .spawn()?;
