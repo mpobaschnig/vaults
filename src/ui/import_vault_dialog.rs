0006658dc97d1d40349935b64e2857659219b9be
Martin Pobaschnig <martin.pobaschnig@protonmail.com>
2021-12-27 19:39:00 +0100

    import_vault_dialog: Set combo_box_text by id
    Don't use arbitrary numbers, but reference to the values by id.

--- src/ui/import_vault_dialog.rs
+++ src/ui/import_vault_dialog.rs
@@ -580,7 +580,9 @@ impl ImportVaultDialog {
 
                                 self_.encrypted_data_directory_info_label.set_visible(true);
 
-                                self_.backend_type_combo_box_text.set_active(Some(1));
+                                self_
+                                    .backend_type_combo_box_text
+                                    .set_active_id(Some("gocryptfs"));
 
                                 return true;
                             }
@@ -592,7 +594,9 @@ impl ImportVaultDialog {
 
                                 self_.encrypted_data_directory_info_label.set_visible(true);
 
-                                self_.backend_type_combo_box_text.set_active(Some(0));
+                                self_
+                                    .backend_type_combo_box_text
+                                    .set_active_id(Some("CryFS"));
 
                                 return true;
                             }
