f72ef21a3f25e1fff90674782fce76bf02983c1c
Martin Pobaschnig <mpobaschnig@protonmail.com>
2023-12-26 21:07:58 +0100

    Add temporary mount switch row
    This adds the switch to use temporary mounts in the UI.

--- src/ui/pages/vaults_page_row_settings_window.rs
+++ src/ui/pages/vaults_page_row_settings_window.rs
@@ -72,6 +72,8 @@ mod imp {
         pub toast_overlay: TemplateChild<adw::ToastOverlay>,
         #[template_child]
         pub lock_screen_switch_row: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub temporary_mount_switch_row: TemplateChild<adw::SwitchRow>,
 
         pub current_vault: RefCell<Option<Vault>>,
         pub to_remove: RefCell<bool>,
@@ -99,6 +101,7 @@ mod imp {
                 mount_directory_error_label: TemplateChild::default(),
                 toast_overlay: TemplateChild::default(),
                 lock_screen_switch_row: TemplateChild::default(),
+                temporary_mount_switch_row: TemplateChild::default(),
                 current_vault: RefCell::new(None),
                 to_remove: RefCell::new(false),
             }
