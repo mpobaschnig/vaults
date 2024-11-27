43a380146a7b41d3e60eae62af91958c2125fc0d
Martin Pobaschnig <mpobaschnig@protonmail.com>
2024-11-27 19:57:31 +0100

    preferences: Port from AdwWindow to AdwDialog

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -7,5 +7,5 @@ mod window;
 
 pub use add_new_vault_window::AddNewVaultWindow;
 pub use import_vault_window::ImportVaultDialog;
-pub use preferences::PreferencesWindow;
+pub use preferences::VaultsSettingsWindow;
 pub use window::ApplicationWindow;
