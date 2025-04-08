e529e338c4f544ef6519b5091ca9d580f0a52659
Martin Pobaschnig <mpobaschnig@protonmail.com>
2025-04-08 22:10:49 +0200

    global_config_convert: Add sem version converting function

--- src/legacy/global_config_converter.rs
+++ src/legacy/global_config_converter.rs
@@ -17,3 +17,57 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+pub fn get_sem_version(current_version: &str) -> Option<(u32, u32, u32)> {
+    let mut version = Vec::new();
+
+    if current_version.contains("-") {
+        let parts: Vec<&str> = current_version.split("-").collect();
+        if let Some(first) = parts.first() {
+            version.push(first.to_string());
+        } else {
+            return None;
+        }
+    } else {
+        version.push(current_version.to_owned());
+    }
+    if version.len() != 1 {
+        return None;
+    }
+
+    let versions: Vec<&str> = version[0].split(".").collect();
+    if versions.len() != 3 {
+        return None;
+    }
+
+    let major = versions[0].parse::<u32>().unwrap();
+    let minor = versions[1].parse::<u32>().unwrap();
+    let patch = versions[2].parse::<u32>().unwrap();
+
+    Some((major, minor, patch))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correct_full() {
+        assert_eq!(get_sem_version("0.0.0"), Some((0, 0, 0)));
+        assert_eq!(get_sem_version("0.9.0"), Some((0, 9, 0)));
+        assert_eq!(get_sem_version("0.10.0"), Some((0, 10, 0)));
+        assert_eq!(get_sem_version("0.11.0"), Some((0, 11, 0)));
+    }
+
+    #[test]
+    fn test_correct_full_dev() {
+        assert_eq!(get_sem_version("0.7.0-920ce7a"), Some((0, 7, 0)));
+        assert_eq!(get_sem_version("0.0.0-abcdef"), Some((0, 0, 0)));
+    }
+
+    #[test]
+    fn test_incorrect() {
+        assert_eq!(get_sem_version(""), None);
+        assert_eq!(get_sem_version("0"), None);
+        assert_eq!(get_sem_version("0.10"), None);
+    }
+}
